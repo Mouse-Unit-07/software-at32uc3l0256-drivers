455e33bb6bf4b7c63e70580b1022ae7f2fe8ee21
Ryuichi Lin <ryuno107lin@gmail.com>
2026-01-30 13:50:57 -0500

    feat: add clock handler elements null test

--- hal_at32uc3l0256_config/tests/test_hal_at32uc3l0256_config.cpp
+++ hal_at32uc3l0256_config/tests/test_hal_at32uc3l0256_config.cpp
@@ -10,6 +10,8 @@
 /*============================================================================*/
 
 extern "C" {
+#include <stdint.h>
+#include "clock_hal.h"
 #include "clock_hal_config.h"
 }
 
@@ -46,4 +48,13 @@ TEST(HalConfigTest, HandlerIsNotNull)
 {
     struct clock_hal_handler *clock_handler = get_clock_hal_handler();
     CHECK(clock_handler != NULL);
+}
+
+TEST(HalConfigTest, HandlerElementsAreNotNull)
+{
+    struct clock_hal_handler *clock_handler = get_clock_hal_handler();
+    CHECK(clock_handler->init_clock != NULL);
+    CHECK(clock_handler->deinit_clock != NULL);
+    CHECK(clock_handler->delay_ms != NULL);
+    CHECK(clock_handler->delay_us != NULL);
 }
